a098fdeb8117313d3dd309e0b6ba90a98c42c117
nat13alia <natalia13krawczyk@gmail.com>
2020-10-22 18:32:30 +0200

    Add project files.

---
+++ Wsei.ExchangeThings.Web/Controllers/ExchangesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Wsei.ExchangeThings.Web.Controllers
+{
+    public class ExchangesController : Controller
+    {
+        public IActionResult Show(string id)
+        {
+            return View();
+        }
+    }
+}
