0d32f002ec14a3605f56eb72d85299d7040e7bb7
nat13alia <natalia13krawczyk@gmail.com>
2020-12-08 16:09:52 +0100

    Lab 4

---
+++ Wsei.ExchangeThings.Web/ViewComponents/LatestItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Wsei.ExchangeThings.Web.Database;
+
+namespace Wsei.ExchangeThings.Web.ViewComponents
+{
+    public class LatestItem : ViewComponent
+    {
+        private readonly ExchangesDbContext _dbContext;
+        public LatestItem(ExchangesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var latestItem = _dbContext.Items
+                .OrderByDescending(x => x.Id).First();
+            return View("Index", latestItem);
+        }
+    }
+}
